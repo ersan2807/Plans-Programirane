3263559b85a7e49db323db879bb12c219f3919bc
ersan2807 <124554664+ersan2807@users.noreply.github.com>
2024-05-15 06:50:49 +0300

    Add files via upload

---
+++ Selection.Sort/Sort 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Selectionsort.Sort(number);
+            Console.WriteLine(string.Join(" ",number));
+        }
+    }
+}
---
+++ Selection.Sort/Sort 1/Selectionsort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_1
+{
+    internal class Selectionsort
+    {
+        public static void Swap<T>(T[] elements,int first, int second)
+        {
+            T temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+        public static void Sort<T>(T[] arr) where T : IComparable
+        {
+            for(int index = 0; index < arr.Length; index++)
+            {
+                int min = index;
+                for(int curr= index+1;curr< arr.Length; curr++)
+                {
+                    if (IsLess(arr[curr], arr[min]))
+                    {
+                        min = curr;
+                    }
+                }
+            }
+        }
+        static bool IsLess(IComparable first,IComparable second)
+        {
+            return first.CompareTo(second) < 0;
+        }
+    }
+}
